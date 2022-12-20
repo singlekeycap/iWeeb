45e6977f8a55aecadc4607da6dfc8ebf0efab483
justanobody2107 <andreadallara.2107@gmail.com>
2022-12-19 19:41:07 -0500

    Initial Commit

---
+++ iWeeb/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iWeeb
+//
+//  Created by Dre Dall'Ara on 12/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iWeeb/iWeebApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWeebApp.swift
+//  iWeeb
+//
+//  Created by Dre Dall'Ara on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct iWeebApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
