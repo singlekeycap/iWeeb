e017a248ae97a1b647ea24ebc08a36d6bddc1d9c
justanobody2107 <andreadallara.2107@gmail.com>
2022-12-25 22:25:08 +0100

    V1.2.1

--- iWeeb/ContentView.swift
+++ iWeeb/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     @State var nsfw = false
     @State var provider = "nekos.best"
     @State var imageData = Data(stringLiteral: "")
+    let endpoints: [String : String] = ["Nekos" : "neko", "Waifus" : "waifu", "Shinobu" : "shinobu", "Megumin" : "megumin", "Bullying" : "bully", "Cuddling" : "cuddle", "Crying" : "cry", "Hugging" : "hug", "Wolf Girls" : "awoo", "Kissing" : "kiss", "Licks" : "lick", "Headpats" : "pat", "Smug" : "smug", "Bonk" : "bonk", "YEET" : "yeet", "Blushing" : "blush", "Smiling" : "smile", "Waving" : "wave", "High five" : "highfive", "Handholding" : "handhold", "Noms" : "nom", "Biting" : "bite", "Bear hug" : "glomp", "Slaps" : "slap", "Killing" : "kill", "Kicks" : "kick", "Happiness" : "happy", "Winking" : "wink", "Poking" : "poke", "Dancing" : "dance", "Cringy" : "cringe"]
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -34,6 +35,13 @@ struct ContentView: View {
             TabView {
                 ImageTab(endpoint: $endpoint, url: $url, saveImage: $saveImage, failed: $failed, nsfw: $nsfw, provider: $provider, imageData: $imageData)
                     .onTapGesture {
+                        if #available(iOS 14, *){
+                            for trans in endpoints {
+                                if trans.key == endpoint {
+                                    endpoint = trans.value
+                                }
+                            }
+                        }
                         if self.nsfw {
                             if provider == "waifu.pics" {
                                 let result = SION(jsonUrlString: "https://api.waifu.pics/nsfw/\(endpoint)")
